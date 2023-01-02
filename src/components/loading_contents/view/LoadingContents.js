b0a1cc38db88eb2c6ee1bbca60e4e147788f2a54
ktkim4 <ktkim@intellicode.co.kr>
2023-01-02 16:27:10 +0900

    게시물 컨텐츠 컴포넌트 생성

---
+++ src/components/loading_contents/view/LoadingContents.js
@@ -0,0 +1,7 @@
+import { LoadingSection } from "../style/LoadingStyle";
+
+const LoadingContents = () => {
+  return <LoadingSection.LoadingFrame></LoadingSection.LoadingFrame>;
+};
+
+export default LoadingContents;
