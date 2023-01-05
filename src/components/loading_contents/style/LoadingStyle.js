6d63513e77fe5ef0cb8f092f9d67ea84016c588d
ktkim4 <ktkim@intellicode.co.kr>
2023-01-05 09:21:48 +0900

    슬라이드 옆 컨텐츠

--- src/components/loading_contents/style/LoadingStyle.js
+++ src/components/loading_contents/style/LoadingStyle.js
@@ -1,15 +1,28 @@
 import styled from "styled-components";
 
 export const LoadingSection = {
+  FirstFrame: styled.div`
+  margin-top:7%;
+   ul {
+      li {flex-basis:calc(94%/2); width:calc(94%/2); cursor:pointer;
+        .img_box {
+          img {}
+        }
+        .type {margin-top:1rem; font-size:1.2rem; color:#a1a0a0;}
+        .tit {margin-top:0.5rem; font-size:2rem; font-weight:600; color:#222222; letter-spacing:-0.9px;}
+        .contents {margin-top:2rem; font-size:1.3rem; color:#666666; font-family:"NotoKr"; white-space:pre; word-break:keep-all;}
+      }
+    }
+  `,
   LoadingFrame: styled.div`
     ul {
-      li {flex-basis:calc(100%/4);
+      li {flex-basis:calc(85%/4);
         .img_box {
           img {}
         }
         .type {}
         .tit {}
-        .sub_tit {}
+        .contents {}
       }
     }
   `,
--- src/components/slide_contents/view/SlideContents.js
+++ src/components/slide_contents/view/SlideContents.js
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Scrollbar, Mousewheel } from "swiper";
 import { SlideSection } from "../style/slideStyle";
+import { LoadingSection } from "../../loading_contents/style/LoadingStyle";
 import images from "../../../resources/img/img";
 
 import "swiper/css";
@@ -28,7 +29,7 @@ const FirstContents = [
     type: "1",
     img: `${images.content1}`,
     title: "2022 한국 디자인 연감",
-    contents: "자부한다. 현재 대한민국에서 이처럼 수많은 디자이너, 다양한 분야의 디자인 결과물을 해마다 한 권의 책으로 집대성한 프로젝트는 <한국 디자인 연감> 밖에 없다고,",
+    contents: "자부한다. 현재 대한민국에서 이처럼 수많은 디자이너, 다양한\n 분야의 디자인 결과물을 해마다 한 권의 책으로 집대성한\n 프로젝트는 <한국 디자인 연감> 밖에 없다고,",
   },
   {
     id: 2,
@@ -76,20 +77,26 @@ const SlideContents = () => {
           )}
         </Swiper>
       </SlideSection.SlideFrame>
-      <div>
-        <ul>
+      <LoadingSection.FirstFrame>
+        <ul className="flex flex_dir_c flex_flow_w flex_ai_c flex_jc_sb">
           {FirstContents.map((contents, index) => 
             <li key={index}>
               <div className="img_box">
                 <img src={contents.img} alt=""/>
               </div>
-              <div className="type">{contents.type}</div>
-              <div className="title">{contents.title}</div>
+              <div className="type">
+                {contents.type === "1" ? "DESIGN" : 
+                  (contents.type === "2" ? "STYLE" : 
+                    (contents.type === "3" ? "CULTURE" :  "LIVING")
+                  ) 
+                }
+               </div>
+              <div className="tit">{contents.title}</div>
               <div className="contents">{contents.contents}</div>
             </li>
           )}
         </ul>
-      </div>
+      </LoadingSection.FirstFrame>
     </>
   );
 };
